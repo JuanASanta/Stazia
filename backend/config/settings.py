363dc284a381fc0b0db1dcb873ce8794f9678f84
JuanASanta <sleonjuanantonio@gmail.com>
2026-05-01 16:50:48 +0200

    Fixin enviroment variable setting

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -17,7 +17,8 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / '.env')
+load_dotenv(BASE_DIR.parent / '.env')
+print("DEBUG DB_HOST:", os.getenv("DB_HOST"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
